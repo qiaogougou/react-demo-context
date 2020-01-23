df0482a952e336d3466e6d0c7689d6c42d5f0639
qiao <qiaoyanying@hotmail.com>
2020-01-23 17:47:31 -0500

    react context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Title extends Component {
+  static contextTypes = {
+    title: PropTypes.string,
+    themeColor: PropTypes.string
+  }
+  render() {
+    console.log(this.props)
+    console.log(this.context)
+    const {themeColor} = this.context
+    return (
+    // <h1>{ this.props.title }</h1>
+      <h1 style={{color: themeColor}}>{ this.context.title }</h1>
+    )
+  }
+}
+
+class Demo extends Component {
+  static contextTypes = {
+    title: PropTypes.string
+  }
+  render(){
+    return (
+      <div>
+        {/* { this.props.title } */}
+        { this.context.title }
+      </div>
+    )
+  }
+}
+
+class Button extends Component {
+  static contextTypes = {
+    title: PropTypes.string,
+    themeColor: PropTypes.string,
+    handleChangeThemeColor: PropTypes.func
+  }
+  render(){
+    const {themeColor, handleChangeThemeColor} = this.context
+    return (
+      <div>
+        <button type="button" style={{color: themeColor}} 
+          onClick={()=>handleChangeThemeColor('red')}
+        > Red</button>
+        <button type="button" style={{color: themeColor}}
+          onClick={()=>handleChangeThemeColor('green')}
+        >Green</button>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  static childContextTypes = {
+    title: PropTypes.string,
+    themeColor: PropTypes.string,
+    handleChangeThemeColor: PropTypes.func
+  }
+  constructor(){
+    super()
+    this.state = {
+      themeColor: ''
+    }
+  }
+  getChildContext(){
+    return {
+      title: 'App transfer Title',
+      themeColor: this.state.themeColor,
+      handleChangeThemeColor: (color)=>this.handleChangeThemeColor(color)
+    }
+  }
+  handleChangeThemeColor(color) {
+    this.setState({
+      themeColor: color
+    })
+  }
+  render(){
+    return (
+      <div>
+        {/* <Demo title=" Title "></Demo> */}
+        <Demo></Demo>
+        <Title></Title>
+        <Button></Button>
+      </div>
+    )
+  }
 }
 
 export default App;
